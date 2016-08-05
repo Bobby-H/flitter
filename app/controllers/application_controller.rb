ee57e13d0f0a50225ffeb3e1db369ba66d398d30
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-08-05 03:52:51 -0400

    fixed issue#1: username issuance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   #call the configured params
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   protected #only app controller and affiliated controllers can access whats below it
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,12 +7,12 @@ class PagesController < ApplicationController
 
   def profile
     #grab the username from the URL as :id
-    if (User.find_by_username(params[:id]))
+
       @username = params[:id]
-    else
+
       #redirect to 404 error
-      redirect_to root_path, :notice => 'user not found!'
-    end
+
+
   end
 
   def explore
