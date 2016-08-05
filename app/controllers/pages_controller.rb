27d7605d42a3b344811b9bbf3349f7b2227aee42
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-08-05 06:24:20 -0400

    posts displaying on profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,12 +7,13 @@ class PagesController < ApplicationController
 
   def profile
     #grab the username from the URL as :id
-
+    if (User.find_by_username(params[:id]))
       @username = params[:id]
-
+    else
       #redirect to 404 error
-
-
+      redirect_to root_path, :notice => "User not found!"
+    end
+    @posts = Post.all #give me all the posts in my database
   end
 
   def explore
